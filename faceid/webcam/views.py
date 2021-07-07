1b8c74e68988cafb7c83b5cf9eb889b2f4d153f2
Dang Duc <ducbk3709@gmail.com>
2021-07-07 14:09:22 +0700

    second

---
+++ faceid/webcam/views.py
@@ -0,0 +1,24 @@
+from unittest.test import loader
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import StreamingHttpResponse
+def index(request):
+    return render(request,'index.html')
+
+def stream():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Error: failed to capture image")
+            break
+
+        cv2.imwrite('demo.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('demo.jpg', 'rb').read() + b'\r\n')
+
+def video_feed(request):
+    return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
