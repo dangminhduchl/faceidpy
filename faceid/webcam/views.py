cdce778f4085ec1032cc5c7e35993dbbe8eb25c1
Dang Duc <ducbk3709@gmail.com>
2021-07-12 14:52:28 +0700

    fourth

--- faceid/webcam/views.py
+++ faceid/webcam/views.py
@@ -1,5 +1,5 @@
 from unittest.test import loader
-
+import cv2
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import StreamingHttpResponse
@@ -8,6 +8,9 @@ def index(request):
 
 def stream():
     cap = cv2.VideoCapture(0)
+    # Khởi tạo bộ phát hiện khuôn mặt
+    faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    num_img = 1
 
     while True:
         ret, frame = cap.read()
@@ -15,10 +18,26 @@ def stream():
         if not ret:
             print("Error: failed to capture image")
             break
+        # Chuyển ảnh sang màu xám
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+        # print(faces)
 
-        cv2.imwrite('demo.jpg', frame)
+        # Lặp qua các khuôn mặt nhận được để hiện thông tin
+        for (x, y, w, h) in faces:
+            print(num_img)
+            if num_img <= 10:
+                file = open("./InputImage/InputImage" + str(num_img) + ".txt", "wb")
+                # mã hoá chán chê rồi save file txt rồi gửi file
+
+                np.save(file, frame)
+                num_img += 1
+            # Vẽ hình chữ nhật quanh mặt
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open('demo.jpg', 'rb').read() + b'\r\n')
+
 
 def video_feed(request):
     return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
